17a0634977601a8aebc31f413f6be4061abdd983
peacemaker-eneji <eohieku@axxess.com>
2026-04-28 15:32:09 +0100

    .

--- BasicAuth.api/obj/Debug/net10.0/BasicAuth.api.AssemblyInfo.cs
+++ BasicAuth.api/obj/Debug/net10.0/BasicAuth.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicAuth.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef2ebda63ed239976c47c82009afedcd372895f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be529314c19166a809207b862266a8b8370f45b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicAuth.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicAuth.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
