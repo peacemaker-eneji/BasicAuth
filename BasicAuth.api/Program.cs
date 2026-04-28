ef2ebda63ed239976c47c82009afedcd372895f1
peacemaker-eneji <eohieku@axxess.com>
2026-04-28 14:36:57 +0100

    updae repo

--- BasicAuth.api/Program.cs
+++ BasicAuth.api/Program.cs
@@ -1,6 +1,20 @@
+using BasicAuth.api;
+using BasicAuth.api.data;
+using BasicAuth.api.dtos;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddValidation();
+string connString = "Data Source=Users.db";
+builder.Services.AddSqlite<UserDbContext>(connString);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/signup", (CreateUserDto userDto) => {
+    return userDto;
+});
+
 
 app.Run();
